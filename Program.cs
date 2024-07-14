6193bb0a029d288f43d13b16d685af776b794b52
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-14 10:51:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("please enter the length : ");
+double length = double.Parse(Console.ReadLine());
+Console.Write("please enter the width : ");
+double width = double.Parse(Console.ReadLine());
+if (length == width)
+{
+    Console.WriteLine("it is square");
+}
+else if (length != width)
+{
+    Console.WriteLine("it isn't square");
+}
+
